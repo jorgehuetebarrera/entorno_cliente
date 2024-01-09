e29da41b8044ea70f527c19d32766c3918159167
jorgehuetebarrera <116200501+jorgehuetebarrera@users.noreply.github.com>
2024-01-09 18:19:07 +0100

    socket.io version 2

--- ejercicios gabri/2º trimestre/24-websocket/src/app.js
+++ ejercicios gabri/2º trimestre/24-websocket/src/app.js
@@ -3,4 +3,6 @@ import express from 'express';
 
 const server = express();
 
+server.use(express.static('public'))
+
 export default server;
--- ejercicios gabri/2º trimestre/24-websocket/src/index.js
+++ ejercicios gabri/2º trimestre/24-websocket/src/index.js
@@ -1,8 +1,18 @@
 import  app  from './app.js';
 import config from './config.js';
+import { Server } from 'socket.io';
 
 const { port } = config;
 
-app.listen(port, () => {
-    console.log('Server listening on port ${port}');
+const server = app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+const io = new Server(server);
+
+io.on('connection', (socket) =>{
+    console.log('A user hass connected');
+    socket.on('disconnect', () => {
+        console.log('A user has disconected');
+    });
 });
