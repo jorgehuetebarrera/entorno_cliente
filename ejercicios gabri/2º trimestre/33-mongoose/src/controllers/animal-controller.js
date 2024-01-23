9e9aa66ba2883b2965fbb167aca759701533f12f
jorgehuetebarrera <116200501+jorgehuetebarrera@users.noreply.github.com>
2024-01-23 19:58:51 +0100

    crud de la aplicacion

--- ejercicios gabri/2º trimestre/33-mongoose/src/controllers/animal-controller.js
+++ ejercicios gabri/2º trimestre/33-mongoose/src/controllers/animal-controller.js
@@ -1,10 +1,50 @@
+import { HttpStatusError } from "common-errors";
 import Animal from "../models/Animal.js";
+import mongoose from 'mongoose';
 
 export async function createAnimal(req, res, next){
   try{
-    const animal = new Animal(req.body);
-    const createdAnimal= await animal.save();
+    const Animal = new Animal(req.body);
+    const createdAnimal= await Animal.save();
     return  res.status(201).send(createdAnimal);
+  }catch(error){
+    next(error);
+  }
+}
+
+export async function getAnimals(req, res, next){
+  try{
+    const results= await Animal.find();
+    return  res.send(results)
+  }catch(error){
+    next(error);
+  }
+}
+
+export async function updateAnimal(req, res, next){
+  try{
+    const { id } = req.params;
+
+    const animal = await Animal.findById(id);
+    Object.assign(animal, req.body)
+
+    const updatedAnimal= await animal.save();
+    return  res.status(201).send(updatedAnimal);
+  }catch(error){
+    next(error);
+  }
+}
+
+
+export async function deletedAnimal(req, res, next){
+  try{
+    const { id } = req.params;
+
+    const deletedAnimal= await Animal.findByIdAndDelete(id);
+
+    if(!deletedAnimal)throw HttpStatusError(404,`Animal ${id} not found`)
+
+    return  res.status(201).send(deletedAnimal);
   }catch(error){
     next(error);
   }
--- ejercicios gabri/2º trimestre/33-mongoose/src/routes/animal-router.js
+++ ejercicios gabri/2º trimestre/33-mongoose/src/routes/animal-router.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { createAnimal } from '../controllers/animal-controller.js';
+import { createAnimal, getAnimals, updateAnimal,deletedAnimal } from '../controllers/animal-controller.js';
 
 const router = Router();
 
+router.get('/', getAnimals)
 router.post('/', createAnimal);
+router.patch('/:id', updateAnimal);
+router.delete('/:id', deletedAnimal)
 
 export default router;
